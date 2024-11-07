a86fd24fbb802aab198a320db7241dcef29f0483
Shamil Habeeb <shamilhabeeb5@gmail.com>
2024-11-07 17:54:23 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -834,20 +834,16 @@ async def auto_filter(client, msg, spoll=False):
         try:
             if settings['auto_delete']:
                 await asyncio.sleep(600)
-                    
-                    await pic_fi.delete()
-                    
-                    await message.delete()
+                await pic_fi.delete()
+               await message.delete()
         except KeyError:
             grpid = await active_connection(str(message.from_user.id))
             await save_group_settings(grpid, 'auto_delete', True)
             settings = await get_settings(message.chat.id)
             if settings['auto_delete']:
-                await asyncio.sleep(600)
-                    
-                    await pic_fi.delete()
-                    
-                    await message.delete()
+                await asyncio.sleep(600) 
+                await pic_fi.delete()
+                await message.delete()
     if spoll:
         await msg.message.delete()
 
